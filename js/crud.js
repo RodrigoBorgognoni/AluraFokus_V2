7319f6fc61656396188cd35e86018a318b9452e2
Rodrigo Borgognoni <borgognonirodrigo@gmail.com>
2024-09-18 21:10:41 -0300

    shows lista de tarefas as it's created

--- js/crud.js
+++ js/crud.js
@@ -1,9 +1,12 @@
 const addTaskBtn = document.querySelector(".app__button--add-task");
 const formAddTask = document.querySelector(".app__form-add-task");
 const textArea = document.querySelector(".app__form-textarea");
+const exibirTarefa = document.querySelector(".app__section-task-list");
 
 //array de tarefas do user
-const tasks = [];
+//JSON.parse() transforma a string do localStorage em um objeto tasks (caso exista já no início)
+//caso não existam tarefas, retorna array vazio
+const tasks = JSON.parse(localStorage.getItem("Tarefas")) || [];
 
 //adiciona e retira classe hidden do text area
 addTaskBtn.addEventListener("click", () => {
@@ -24,33 +27,38 @@ formAddTask.addEventListener("submit", (event) => {
     };
 
     tasks.push(tarefa);
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    exibirTarefa.append(elementoTarefa);
 
     //JSON.stringify transforma o array tasks em ums string
     //desse modo o método não retorna [object Object]
     //localStorage trabalha apenas com string
     localStorage.setItem("Tarefas", JSON.stringify(tasks));
+
+    textArea.value = "";
+    formAddTask.classList.add("hidden");
 });
 
 //função para criar tarefa no layout pré-estabelecido
-function criarTarefa(tarefa) {
+function criarElementoTarefa(tarefa) {
     const lista = document.createElement("li");
     lista.classList.add("app__section-task-list-item");
 
     const svg = document.createElement("svg");
     svg.innerHTML = `
-    <svg>
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
-    </svg>
     `;
 
     const paragrafro = document.createElement("p");
-    //paragrafro.classList.add("app__section-task-list-item-description");
+    paragrafro.classList.add("app__section-task-list-item-description");
     paragrafro.textContent = tarefa.descricao;
 
     const botao = document.createElement("button");
+    botao.classList.add("app_button-edit");
+
     const imgBotao = document.createElement("img");
     imgBotao.setAttribute("src", "/imagens/edit.png");
 
@@ -60,4 +68,13 @@ function criarTarefa(tarefa) {
     lista.append(svg);
     lista.append(paragrafro);
     lista.append(botao);
+
+    //cria o elemento lista(e seus appends) e retorna
+    return lista;
 }
+
+//para cada tarefa inserida no array, cria uma tarefa para exibir
+tasks.forEach((tarefa) => {
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    exibirTarefa.append(elementoTarefa);
+});
